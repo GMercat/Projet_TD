1bd99e36d0a3493b726cc1ce47eea7f1ccfec3a6
GMercat <guillaume.mercat@gmail.com>
2013-01-04 12:47:56 +0100

    Naissance de la class Mur qui dérive de la class Case A terme Tour dérivera aussi de la class Case

---
+++ src/Mur.h
@@ -0,0 +1,15 @@
+#ifndef _MUR_H_
+#define _MUR_H_
+
+#include "Defined.h"
+#include "Case.h"
+
+class CMur : public CCase
+{
+public:
+   // Constructeur et destructeur
+   CMur (void);
+   ~CMur (void);
+};
+
+#endif
